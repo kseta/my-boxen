1ed2dc2f72e9f8ec9f28f0a645d24b5a7eef3749
kseta <keigo.seta@gmail.com>
2013-06-02 23:11:41 +0900

    Fix setting for vim, zsh, and some dotfiles

--- modules/people/manifests/kseta.pp
+++ modules/people/manifests/kseta.pp
@@ -6,4 +6,62 @@ class people::kseta {
   include java
   include googledrive
   include alfred
+
+
+  # install with homebrew
+  package {
+    [
+      'ctags',
+      'lv',
+      'mysql',
+      'redis',
+      'wget',
+      'z',
+      'zsh',
+    ]:
+  }
+
+  # install mac applications
+  package {
+    'GoogleJapaneseInput':
+      source => "http://dl.google.com/japanese-ime/latest/GoogleJapaneseInput.dmg",
+      provider => pkgdmg;
+  }
+
+  $home      = "/Users/${::luser}"
+  $src       = "${home}/src"
+
+  # settings for dotfiles
+  $dotfiles  = "${src}/dotfiles"
+  repository { $dotfiles:
+    source  => "${::luser}/dotfiles",
+    require => File[$src]
+  }
+  exec { "sh ${dotfiles}/configure":
+    cwd => $dotfiles,
+    creates => [ "${home}/.zshrc", "${home}/.vimrc", "${home}/.gitconfig", "${home}/.tmux.conf" ],
+    require => Repository[$dotfiles],
+  }
+
+  # seettings for vim
+  $vim         = "${home}/.vim"
+  $neobundle   = "${vim}/bundle/neobundle.vim"
+  file { $vim:
+    ensure => "directory",
+  }
+  repository { $neobundle:
+    source => "Shougo/neobundle.vim",
+    require => File[$vim]
+  }
+
+  # settings for zsh
+  file_line { 'add zsh to /etc/shells':
+    path    => '/etc/shells',
+    line    => "${boxen::config::homebrewdir}/bin/zsh",
+    require => Package['zsh'],
+    before  => Osx_chsh[$::luser];
+  }
+  osx_chsh { $::luser:
+    shell   => "${boxen::config::homebrewdir}/bin/zsh";
+  }
 }
